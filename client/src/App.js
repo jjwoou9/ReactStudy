8b2a9e631c7e53c198d93842a6e1ee21109dcb8e
jeewoou <51470001+jjwoou9@users.noreply.github.com>
2022-07-19 00:14:33 +0900

    mern stack before distribute

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,52 @@
-import React from 'react'
-import './App.css';
-import {Routes, Route } from 'react-router-dom';
-import Heading from "./Component/Heading"
-import List from "./Component/Post/List"
-import Upload from "./Component/Post/Upload"
-import Detail  from './Component/Post/Detail';
-import Edit from './Component/Post/Edit';
+import React, { useEffect } from "react";
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { loginUser, clearUser } from "./Reducer/userSlice.js";
+import firebase from "./firebase.js";
 
-function App() {    
+import Heading from "./Component/Heading";
+import MainPage from "./Component/MainPage";
+
+import Upload from "./Component/Post/Upload";
+import PostArea from "./Component/Post/PostArea";
+import Edit from "./Component/Post/Edit";
+
+import Login from "./Component/User/Login";
+import Register from "./Component/User/Register";
+import MyPage from "./Component/User/MyPage";
+
+function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    firebase.auth().onAuthStateChanged((userInfo) => {
+      // console.log("userInfo ", userInfo);
+      if (userInfo !== null) {
+        dispatch(loginUser(userInfo.multiFactor.user));
+      } else {
+        dispatch(clearUser());
+      }
+    });
+  }, []);
+
+  useEffect(() => {
+    // firebase.auth().signOut();
+  }, []);
+
   return (
     <>
       <Heading />
       <Routes>
-        <Route path="/" element= { <List />} />
-        <Route path="/Upload" element= { <Upload />} />
-        <Route path="/post/:postNum" element= { <Detail />} />
-        <Route path="/edit/:postNum" element= { <Edit />} />
+        <Route path="/" element={<MainPage />} />
+        {/*Post, Reple*/}
+        <Route path="/Upload" element={<Upload />} />
+        <Route path="/post/:postNum" element={<PostArea />} />
+        <Route path="/edit/:postNum" element={<Edit />} />
+        {/*User*/}
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/mypage" element={<MyPage />} />
       </Routes>
     </>
   );
